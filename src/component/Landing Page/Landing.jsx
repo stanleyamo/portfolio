1c97a5fe7ba080e3c5d3af6322030bde9284f115
Stanleyamo <stanleyamobrown3@gmail.com>
2025-10-24 08:40:09 +0000

    router

--- src/component/Landing Page/Landing.jsx
+++ src/component/Landing Page/Landing.jsx
@@ -6,8 +6,8 @@ const Landing = () => {
     return (
         <div className='hero'>
             <img src={avatar} alt="profile"/>
-            <h1><span>I'm Stanley Amo-Brown,</span> software engineer based in Ghana.</h1>
-            <p>I'm a software engineer from Accra</p>
+            <h1><span>I'm Stanley Amo-Brown,</span> software developer based in Ghana.</h1>
+            <p>I'm a software developer from Accra</p>
             <div className="hero-action">
                 <div className="hero-connect">Connect with me</div>
                 <div className="hero-resume">My resume</div>
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import './Navbar.css'
 import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const [darkMode, setDarkMode] = useState(false);
@@ -13,10 +14,10 @@ const Navbar = () => {
         <div className="navbar">
             <img src={logo} alt="logo"/>
             <ul className="nav-menu">
-                <li><a href="">Home</a></li>
-                <li><a href="">About Me</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">Portfolio</a></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/About'>About Me</Link></li>
+                <li><Link to='/'>Services</Link></li>
+                <li><Link to='/'>Portfolio</Link></li>
                 <li><a href="">Contact</a></li>
             </ul>
 
