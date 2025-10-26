93ed5e5d9cbc203f67fc84b07128ffa4a1f0e75c
Stanleyamo <stanleyamobrown3@gmail.com>
2025-10-26 22:01:32 +0000

    minor

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect} from 'react';
 import './Navbar.css'
-import logo from '../../assets/logo.svg';
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
